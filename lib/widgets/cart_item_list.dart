d95e3f403b4dea195f63c2692395f5d814efcfeb
wxiaoxuan <92334207+wxiaoxuan@users.noreply.github.com>
2023-11-09 16:43:55 +0800

    Revert "Extract ProcessCartData into function & Extract Cart's MenuItem List + Header into Separate Component & Added Empty Cart Display"
    This reverts commit f5bdbc40bc637ceac30590fb52e9b6915e8427e4.

---
+++ lib/widgets/cart_item_list.dart
@@ -0,0 +1,122 @@
+// In Shopping Cart Page
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/cart_provider.dart';
+
+class CartItemList extends StatefulWidget {
+  const CartItemList({super.key});
+
+  @override
+  State<CartItemList> createState() => _CartItemListState();
+}
+
+class _CartItemListState extends State<CartItemList> {
+  @override
+  Widget build(BuildContext context) {
+    final cartProvider = Provider.of<CartProvider>(context, listen: false);
+
+    // Calculate Total Price of Menu Items in the Cart
+    double totalCartPrice = cartProvider.calculateTotalPrice();
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.67,
+          child: Consumer<CartProvider>(
+            builder: (context, cartProvider, child) {
+              return ListView.builder(
+                itemCount: cartProvider.cartItems.length,
+                itemBuilder: (context, index) {
+                  final menuItem = cartProvider.cartItems[index];
+
+                  return Card(
+                    elevation: 1,
+                    color: Colors.white,
+                    child: ListTile(
+                      title: Text(
+                        menuItem['menuTitle'],
+                        style: const TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      subtitle: Row(
+                        children: [
+                          const Text('Quantity: '),
+                          // Reduce the quantity
+                          IconButton(
+                            icon: const Icon(Icons.remove),
+                            iconSize: 16.0,
+                            onPressed: () {
+                              if (menuItem['quantity'] > 1) {
+                                cartProvider.updateCartItemQuantity(
+                                  menuItem['menuID'],
+                                  menuItem['quantity'] - 1,
+                                );
+                                // Recalculate the total price
+                                totalCartPrice =
+                                    cartProvider.calculateTotalPrice();
+                                setState(() {});
+                              }
+                            },
+                          ),
+                          // Update the displayed quantity based on the updated value in the cart
+                          Container(
+                            width: 30.0,
+                            alignment: Alignment.center,
+                            color: Colors.white,
+                            child: Text(
+                              menuItem['quantity'].toString(),
+                            ),
+                          ),
+                          // Increase the quantity
+                          IconButton(
+                            icon: const Icon(Icons.add),
+                            iconSize: 16.0,
+                            onPressed: () {
+                              cartProvider.updateCartItemQuantity(
+                                menuItem['menuID'],
+                                menuItem['quantity'] + 1,
+                              );
+                              // Recalculate the total price
+                              totalCartPrice =
+                                  cartProvider.calculateTotalPrice();
+                              setState(() {});
+                            },
+                          ),
+                        ],
+                      ),
+                      // Total Price for Each Menu Item + Delete Icon
+                      trailing: SizedBox(
+                        width: 120.0, // Adjust the width as needed
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                '\$${cartProvider.calculateTotalItemPrice(menuItem).toStringAsFixed(2)}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                cartProvider.removeCartItem(menuItem['menuID']);
+                                setState(() {});
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
